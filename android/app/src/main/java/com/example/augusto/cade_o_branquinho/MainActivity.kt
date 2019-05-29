31411edf8f450f66c72b7fbb26545722f7799eed
Augusto Boranga <gutoboranga@gmail.com>
2019-05-28 23:17:21 -0300

    feat: change toolbar title

--- android/app/src/main/java/com/example/augusto/cade_o_branquinho/MainActivity.kt
+++ android/app/src/main/java/com/example/augusto/cade_o_branquinho/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.support.v7.widget.Toolbar
 import com.example.augusto.cade_o_branquinho.fragments.MapFragment
 import com.example.augusto.cade_o_branquinho.fragments.TimesFragment
 import com.example.augusto.cade_o_branquinho.fragments.WarningsFragment
@@ -19,12 +20,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         when (item.itemId) {
             R.id.navigation_map -> {
+                showMapsToolbar()
                 transaction.replace(R.id.fragment_container, mapFragment)
             }
             R.id.navigation_warnings -> {
+                showWarningsToolbar()
                 transaction.replace(R.id.fragment_container, warningsFragment)
             }
             R.id.navigation_times -> {
+                showTimesToolbar()
                 transaction.replace(R.id.fragment_container, timesFragment)
             }
         }
@@ -39,7 +43,25 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         val navigation = findViewById<BottomNavigationView>(R.id.navigation)
         navigation.setOnNavigationItemSelectedListener(this)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
         navigation.selectedItemId = R.id.navigation_map
+    }
+
+    // --- Toolbar functions ---
+
+    fun showMapsToolbar() {
+        supportActionBar!!.title = "Cadê o branquinho?"
+    }
+
+    fun showWarningsToolbar() {
+        supportActionBar!!.title = "Avisos"
+    }
+
+    fun showTimesToolbar() {
+        supportActionBar!!.title = "Horários"
     }
 
 }
