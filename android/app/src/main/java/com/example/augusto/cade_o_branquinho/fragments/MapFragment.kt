a4de9c55b969d2445ea304c63d0a29b9a6a9eebe
Augusto Boranga <gutoboranga@gmail.com>
2019-06-01 01:17:48 -0300

    start implementing map fragment

--- android/app/src/main/java/com/example/augusto/cade_o_branquinho/fragments/MapFragment.kt
+++ android/app/src/main/java/com/example/augusto/cade_o_branquinho/fragments/MapFragment.kt
@@ -7,10 +7,42 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.augusto.cade_o_branquinho.R
 
-class MapFragment : Fragment() {
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.map_fragment_layout, container, false)
+        val view = inflater.inflate(R.layout.map_fragment_layout, container, false)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = childFragmentManager.findFragmentById(R.id.maps_fragment_support_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
+    }
+
+    // --- Map methods
+
+    override fun onMapReady(p0: GoogleMap?) {
+        map = p0!!
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
 }
