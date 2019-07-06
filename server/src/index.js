fab220c6943b4b4c6d528ffd1f2262ed3253ffc0
José Pedro Martinez <jpsmartinez@inf.ufrgs.br>
2019-07-06 18:57:06 -0300

    nested callbacks so parsed status messages are returned

--- server/src/index.js
+++ server/src/index.js
@@ -6,35 +6,42 @@ var scrapper = require('../scrapper/run.js');
 
 const app = express();
 
-function receive_alerts(alerts) {
+function parse_msg(msg) {
+  /* Given a (unparsed) message object from scrapper module, 
+      returns it as a JSON with the extra key 'status', 
+        indicating whether the message text is positive, negative or undefined
+  */ 
+
+  let has_not = msg.toLowerCase().search("não") >= 0;
+  let has_circula = msg.toLowerCase().search("circula") >= 0;
+  let has_manutencao = msg.toLowerCase().search("manutenção") >= 0;
+  let has_normalmente = msg.toLowerCase().search("normalmente") >= 0;
+
+  let off = (has_manutencao) || (has_not && has_circula);
+  let on = (has_circula && (has_normalmente || !has_not));
+  if (on && off) {
+    console.log("We have a on-and-off problem");
+  }
+
+  let obj = JSON.parse(msg);
+  if (on) {
+    obj["status"] = "on";
+  }
+  else if (off) {
+    obj["status"] = "off";
+  }
+  else {
+    obj["status"] = "undefined";
+  }
+  return obj;
+}
+
+function receive_alerts(alerts, cb) {
   let statuses = [];
   for (let i=0;  i < alerts.length; i++) {
-    let msg = alerts[i];
-
-    let has_not = msg.toLowerCase().search("não") >= 0;
-    let has_circula = msg.toLowerCase().search("circula") >= 0;
-    let has_manutencao = msg.toLowerCase().search("manutenção") >= 0;
-    let has_normalmente = msg.toLowerCase().search("normalmente") >= 0;
-
-    let off = (has_manutencao) || (has_not && has_circula);
-    let on = (has_circula && (has_normalmente || !has_not));
-    if (on && off) {
-      console.log("We have a on-and-off problem");
-    }
-
-    let obj = JSON.parse(msg);
-    if (on) {
-      obj["status"] = "on";
-    }
-    else if (off) {
-      obj["status"] = "off";
-    }
-    else {
-      obj["status"] = "undefined";
-    }
-    statuses.push(obj);
+    statuses.push(parse_msg(alerts[i]));
   }
-  console.log(statuses);
+  cb(statuses);
 }
 
 app.get('/', (req, res) => {
@@ -42,11 +49,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/status', (req, res) => {
-  scrapper.getAlerts(receive_alerts); // how to get the results?
-
-  res.send({
-    'status': true ? 'on' : 'off'
-  })
+  scrapper.getAlerts(function(alerts) { 
+    receive_alerts(alerts, function(statuses) { 
+      res.send({
+        "past_statuses": statuses,
+        "current_status": statuses[0]['status']
+      });
+    });
+  });
 })
 
 app.listen(process.env.PORT, () =>
